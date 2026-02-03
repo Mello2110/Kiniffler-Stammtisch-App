e503736ec2c5dc71e22070fed8f7cc8b4400f683
Mello <you@example.com>
2026-02-03 23:49:52 +0100

    Fix Push Notifications (SW Config, VAPID, Race Condition) and Redesign Options Page

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,15 +1,15 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+// Initialize the Firebase app in the service worker
 firebase.initializeApp({
-    messagingSenderId: "1071465839210" // This is a public ID safe to expose? Wait, checking firebase.ts...
-    // I should probably not hardcode the ID if possible, but SW doesn't have access to process.env at build time easily without webpack injection.
-    // However, for this task, I will use a placeholder or try to read from a shared config if possible. 
-    // Given the constraints and standard Next.js public folder behavior, hardcoding or using a script to inject is common.
-    // I will look at firebase.ts again to see the env var name: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
-    // I will try to use a fetch to get config or just trust the user will fill it.
-    // Actually, usually we put the config object here.
+    apiKey: "AIzaSyCFa09-8DVF8o2OQssr15lZJ-aCIvh21HM",
+    authDomain: "webapp-stammtisch.firebaseapp.com",
+    projectId: "webapp-stammtisch",
+    storageBucket: "webapp-stammtisch.firebasestorage.app",
+    messagingSenderId: "112239637478",
+    appId: "1:112239637478:web:78a18a3671f33605801c27",
+    measurementId: "G-HGCS3PGQDY"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
